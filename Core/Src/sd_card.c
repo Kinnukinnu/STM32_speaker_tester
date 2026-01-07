cee846cbc6fcaae99b5d75d6dcf76d2582bdff75
Kinnukinnu <janne.kinnu@gmail.com>
2026-01-07 10:47:53 +0200

    writing a test file to sd card works

--- Core/Src/sd_card.c
+++ Core/Src/sd_card.c
@@ -12,9 +12,15 @@
 
 static FRESULT sd_result;
 static FATFS sdCard;
+static FIL testFile;
 volatile int debug_error_code = 0;
 
 int sd_card_init(){
+
+	uint8_t file_name[] = "test.txt";
+	uint8_t temp_number;
+	uint8_t test_text[] = "Hello I am working";
+
 
 	//mounting card
 	sd_result = f_mount(&sdCard,SDPath, 1);
@@ -29,8 +35,38 @@ int sd_card_init(){
 	else
 	{
 		printf("SD card successfully mounted\n");
-		return 0;
 	}
 
+	sd_result = f_open(&testFile ,(void*)file_name, FA_WRITE|FA_CREATE_ALWAYS);
+	if(sd_result != 0)
+	{
+		debug_error_code = 21;
+		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		HAL_Delay(10000);
+
+		while(1);
+	}
+	else{
+		debug_error_code = 20;
+	}
+
+	//writing to the file
+	sd_result = f_write(&testFile, (void*)test_text, (UINT)sizeof(test_text), (UINT*)&temp_number);
+	if(sd_result != 0)
+	{
+		debug_error_code = 31;
+		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		HAL_Delay(5000);
+		return 1;
+	}
+	else
+	{
+		debug_error_code = 30;
+
+
+
+	}
+	f_close(&testFile);
+	return 0;
 }
 
